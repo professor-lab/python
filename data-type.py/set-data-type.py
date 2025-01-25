c7c46b3cf38512e12065671059b4eda3579a4104
professor-lab <arjansambad@gmail.com>
2025-01-25 15:44:18 +0530

    create set data type

---
+++ data-type.py/set-data-type.py
@@ -0,0 +1,18 @@
+
+# Set
+
+# dublicate value will be  remove
+basket={'shakti','mehul','bhavesh','harsha','darshan','harsha'}
+print(basket) 
+
+# unique letter in a
+a=set("professor-shakti")
+print(a)
+
+# add value  in set
+a.add('Z')
+print(a)
+
+#Frozen set
+b=frozenset({'ss','hh','aa','kk','tt','ii'})
+print(b)
