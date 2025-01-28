e849daefab37d75c7c04219c181030ac5f8ab95b
professor-lab <arjansambad@gmail.com>
2025-01-28 15:54:46 +0530

    return value function

---
+++ function/return-value-function.py
@@ -0,0 +1,12 @@
+def revafu(x):
+    return 5*x
+print(revafu(1))
+print(revafu(2))
+print(revafu(3))
+print(revafu(4))
+print(revafu(5))
+print(revafu(6))
+print(revafu(7))
+print(revafu(8))
+print(revafu(9))
+print(revafu(10))
