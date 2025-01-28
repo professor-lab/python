0b54713523d63502fbae94684dbead42274d08cd
professor-lab <arjansambad@gmail.com>
2025-01-28 09:17:03 +0530

    sum with function

--- function/fun-sum.py
+++ function/fun-sum.py
@@ -2,3 +2,15 @@ def sum(a,b):
     total=a+b
     print(total)
 sum(2,3)
+
+sum(10,10000)
+sum(10,50.56)
+
+def sumr(a,b):
+    return a+b
+total2=sumr(10,20)
+print(total2)
+total3=sumr(10.20,56.23)
+print(total3)
+total4=sumr(45.99,99.00)
+print(total4)
