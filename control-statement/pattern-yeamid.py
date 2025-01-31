887406326dc8352429cf76bf4fb4d54542e770b7
professor-lab <arjansambad@gmail.com>
2025-01-31 15:57:47 +0530

    aplphabet

--- control-statement/pattern-yeamid.py
+++ control-statement/pattern-yeamid.py
@@ -1,5 +1,5 @@
 #Outer for loop to handle number of rows  
-for i in range(0, rows):  
+for i in range(0, 5):  
     #Inner for loop to handle number of columns  
     #values change according to the outer loop  
         for j in range(0, i + 1):  
