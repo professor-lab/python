ab995f4f6fd805280f3ac8e9b2c8887e7b932560
professor-lab <arjansambad@gmail.com>
2025-02-01 16:19:15 +0530

    same name object

--- class-object/attribute.py
+++ class-object/attribute.py
@@ -1,7 +1,10 @@
 class Attribute:
     college_name="abc college"
+    name="anonymos"
     def __init__(self,name):
         self.name=name
         print(name)
+
 s1=Attribute("professor")
+print(s1.name)
 print(s1.college_name)
