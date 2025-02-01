9ae0fe80ce8516b9d35c19c0fcd5bbe5437b2ffd
professor-lab <arjansambad@gmail.com>
2025-02-01 16:25:28 +0530

    method update

--- class-object/method.py
+++ class-object/method.py
@@ -1,12 +1,17 @@
 class Student:
 
-    def __init__(self,name):
+    def __init__(self,name,course):
         self.name=name
+        self.course=course
     def hello(self):
         print("hello",self.name)
+    def get_course(self):
+        print(self.name,"Course Is",self.course)
 
-s1=Student("professor")
-s1.hello()        
+s1=Student("professor","BCA")
+s1.hello()   
+s1.get_course()     
 
-s2=Student("teacher")
+s2=Student("teacher","BA")
 s2.hello()
+s2.get_course()
