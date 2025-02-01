3ddba28227341b46740c63ddff00600113d50adf
professor-lab <arjansambad@gmail.com>
2025-02-01 16:11:28 +0530

    multiple variable

--- class-object/contructor.py
+++ class-object/contructor.py
@@ -1,11 +1,12 @@
 class Student:
 
-    def __init__(self,name):
+    def __init__(self,name,mark):
         self.name=name
+        self.mark=mark
         print("add new student in database")
 
-s1=Student("professor")
-print(s1.name)
+s1=Student("professor",98)
+print(s1.name,s1.mark)
 
-s1=Student("teacher")
-print(s1.name)
+s2=Student("teacher",97)
+print(s2.name,s2.mark)
